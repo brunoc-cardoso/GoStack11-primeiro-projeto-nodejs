44309b7b9406e08ca4e7a1c9cc5367cd4fb949d0
bruno <brunocpessoal@gmail.com>
2020-06-15 16:17:27 -0300

    created and configured the project startpoint

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const PORT = 3333;
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on port ${PORT}`)
+});
