11948a4014ab0ec9ef9948bea3e5edab7d6779f8
bruno <brunocpessoal@gmail.com>
2020-06-15 20:07:45 -0300

    fix .ts file import error

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const PORT = 3333;
-
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server started on port ${PORT}`);
