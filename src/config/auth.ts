539acf3718f8ba4af8c1661a31f11d2712e6f9ad
bruno <brunocpessoal@gmail.com>
2020-06-18 19:54:24 -0300

    add verification with token on routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4bacf756141de32a47fb11405abd1e2b',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 
 import { sign } from 'jsonwebtoken';
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface RequestDTO {
   email: string;
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '4bacf756141de32a47fb11405abd1e2b', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
